ea67cad9ca7f63215a609f3921a261269b17f38c
Radyslav Dmytrenko <sheo.pro.corp@gmail.com>
2019-12-23 22:21:52 +0200

    burger-menu

--- define-clinic-academy/app/js/script.js
+++ define-clinic-academy/app/js/script.js
@@ -2,4 +2,7 @@ $(".burger-menu").on('click', function (e) {
     e.preventDefault()
     $('.burger-menu__btn').toggleClass('active')
     $('.top-menu').toggleClass('active')
+    $('.burger-menu__btn span::before').toggleClass('active')
+    $('.burger-menu__btn span::after').toggleClass('active')
+
 })
