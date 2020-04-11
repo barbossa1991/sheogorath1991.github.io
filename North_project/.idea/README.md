06f4050d1ce3093d13e718e61a4f4f5600f01645
Radyslav Dmytrenko <sheo.pro.corp@gmail.com>
2020-04-11 12:06:47 +0300

    beta

--- North_project/.idea/README.md
+++
@@ -1,1 +0,0 @@
-# north.projects.githup.io
--- North_project/.idea/app/js/burger.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.roof__burger').click(function(event) {
-        $('.roof__burger, .roof-menu').toggleClass('active');
-        $('body').toggleClass('lock')
-    })
-});
