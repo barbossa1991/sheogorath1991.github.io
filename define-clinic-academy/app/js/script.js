d03620bf8b7abde7bcf7d2ecc6616a802032ee6c
Radyslav Dmytrenko <1991barbossa@gmail.com>
2020-06-23 09:48:29 +0300

    bug fixes

--- define-clinic-academy/app/js/script.js
+++
@@ -1,8 +0,0 @@
-$(".burger-menu").on('click', function (e) {
-    e.preventDefault()
-    $('.burger-menu__btn').toggleClass('active')
-    $('.top-menu').toggleClass('active')
-    $('.burger-menu__btn span::before').toggleClass('active')
-    $('.burger-menu__btn span::after').toggleClass('active')
-
-})
--- define-clinic-academy/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('sass', function(){
-  return gulp.src('app/sass/style.sass')
-  .pipe(sass())
-  .pipe(gulp.dest('app/css'))
-})
